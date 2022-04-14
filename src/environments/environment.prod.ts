084400878899939159ae2a20adce23493813fe14
Ramazan Baybörek <61885368+androloper@users.noreply.github.com>
2022-04-14 08:38:18 +0300

    config file has deleted to share this repo in public

--- src/environments/environment.prod.ts
+++
@@ -1,8 +0,0 @@
-export const environment = {
-    production: true,
-    rootUrl : "http://izersoft.online/api",
-    taskManagementUrl : "http://izersoft.online/api/TaskManagementApi",
-    identityServerUrl: "http://izersoft.online/api/IdentityServer",
-    vehicleTrackingUrl: "http://izersoft.online/api/VehicleTrackingSystemApi",
-    hubUrl: 'http://izersoft.online/VehicleTcpServerHub',
-};
